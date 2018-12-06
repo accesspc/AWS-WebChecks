5bfc36976a632336b162512f89d939a8a38a30f3
root <root@ip-172-31-2-42.eu-west-2.compute.internal>
2018-12-06 14:17:40 +0000

    Install fixes

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -x
 
 apt install -y python3-pip
-pip3 install boto3
+pip3 install -r requirements.txt
 
 install -m 0644 cron.d/webchecks /etc/cron.d/webchecks
 install -m 0644 logrotate.d/webchecks /etc/logrotate.d/webchecks
